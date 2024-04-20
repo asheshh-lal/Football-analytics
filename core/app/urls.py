568aadba52e5625165f0b1ea931f06b2e2b973e0
Ashesh Lal <87692027+asheshh-lal@users.noreply.github.com>
2024-04-20 09:41:59 -0400

    Django changes

--- core/app/urls.py
+++ core/app/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-import app
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include("app.urls"))
+    path('', views.generate_chart1_data, name='display_base'),
 ]
--- core/app/views.py
+++ core/app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+# import plotly.express as px
+from django.db.models import Sum, F, Count
+import math
+import pandas as pd
+# import plotly.graph_objs as go
+# from plotly.subplots import make_subplots
+# import seaborn as sns
+# import joblib
 
 # Create your views here.
+
+def generate_chart1_data(request):
+    file_path = "/Users/asheshlalshrestha/Desktop/Datanal/Project/Football-analytics/event/Generated Datsets/match.csv"
+    
+    # Check if the file path is valid
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        return render(request, 'error.html', {'message': 'File not found'})
+
+    # Process the data as needed
+    # chart_data = df.head().to_dict(orient='records')
+    chart_data= df.head()
+
+    return render(request, 'app/chart1.html', {'chart_data': chart_data})
